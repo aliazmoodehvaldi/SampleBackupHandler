046d035e5a366cb609848388b53275b7adf823cb
Ali Azmoodeh Valdi <treeroot.ir@gmail.com>
2025-09-20 14:14:36 +0330

    Bugfix: stop mongodb container

--- src/make-backup.sh
+++ src/make-backup.sh
@@ -11,25 +11,33 @@ mongodb_name="${MONGODB_DOCKER_NAME:-mongo}"
 
 # Check if the mongo container exists
 if sudo docker ps -a --format '{{.Names}}' | grep -q "^$mongodb_name$"; then
-	# If the container exists, execute mongodump
-	sudo docker exec $mongodb_name mongodump \
-		--verbose \
-		--archive=$ARCHIVE_MONGODB_PATH \
-		--authenticationDatabase admin \
-		--port $MONGODB_PORT \
-		-u $MONGODB_USERNAME \
-		-p $MONGODB_PASSWORD
+    echo "Stopping MongoDB container..."
+    sudo docker stop $mongodb_name
+
+    echo "Taking MongoDB backup..."
+    sudo docker exec $mongodb_name mongodump \
+        --verbose \
+        --archive=$ARCHIVE_MONGODB_PATH \
+        --authenticationDatabase admin \
+        --port $MONGODB_PORT \
+        -u $MONGODB_USERNAME \
+        -p $MONGODB_PASSWORD
+
+    echo "Starting MongoDB container..."
+    sudo docker start $mongodb_name
 else
-	# If the container doesn't exist, print an error message
-	echo "${yellow}Warning: 'mongo' docker container does not exist Or mongodb container have different name, Set MONGODB_DOCKER_NAME env on exclude.txt file"
+    echo "${yellow}Warning: 'mongo' docker container does not exist Or mongodb container have different name, Set MONGODB_DOCKER_NAME env on exclude.txt file"
 fi
 
+# Create tar backup
 if [ -f "$base_path/exclude.txt" ]; then
-	tar -czvf $backup_path --exclude-from="$base_path/exclude.txt" $TARGET_PATH
+  tar -czvf $backup_path --exclude-from="$base_path/exclude.txt" $TARGET_PATH
 else
-	tar -czvf $backup_path $TARGET_PATH
+    tar -czvf $backup_path $TARGET_PATH
 fi
 
+# Upload backup
 bash "$base_path/upload-file.sh" $1 $backup_path
 
+# Remove local backup
 rm $backup_path
